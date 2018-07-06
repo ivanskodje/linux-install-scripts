35a2ce657c928548cf4b84e965233e1e835e9470
Ivan Skodje <ivanskodje@protonmail.com>
2018-07-06 05:38:31 +0200

    Added script to create new user in CentOS, and swapped the order they should be run with disable root access.

--- CentOS 7.5.1804/01-initial-server-setup.sh
+++ CentOS 7.5.1804/01-initial-server-setup.sh
@@ -9,11 +9,18 @@ yum update -y && yum upgrade -y
 # Install nano text editor
 yum install nano -y
 
+# Install wget (used to download files from the web)
+yum install wget -y
+
 # Install GIT
 yum install git -y
 
 # Install Tree (in order to view file directory structure by using the command)
 yum install tree -y
+
+# Install GCC and Java
+# yum install gcc -y
+# yum install java
 
 # Setup a keepalive interval for clients and the server
 echo -e "\nClientAliveInterval 15" >> /etc/ssh/sshd_config
