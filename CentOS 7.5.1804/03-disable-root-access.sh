c17ef90e634741810cb5c85037193e91c9fce8d2
Ivan Skodje <ivanskodje@users.noreply.github.com>
2018-07-11 13:24:34 +0200

    SSHD Service restart
    In order for the changes to take effect, we need to restart the sshd service.

--- CentOS 7.5.1804/03-disable-root-access.sh
+++ CentOS 7.5.1804/03-disable-root-access.sh
@@ -1,5 +1,12 @@
+#######################################################################################
+# NB: Make sure you have created another user with root permissions (in group wheel)!
+#######################################################################################
+
 # Create a backup file
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup_$(date +'%Y%m%d')
 
 # Replace #PermitRootLogin yes with PermitRootLogin no
 sed -e 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config > /sshd_config_tmp && mv /sshd_config_tmp /etc/ssh/sshd_config
+
+# Restart sshd service
+systemctl restart sshd.service
