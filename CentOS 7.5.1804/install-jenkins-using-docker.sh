0ae2443df80876c624fb263a3a77f6df8f13bcd9
Ivan Skodje <ivanskodje@protonmail.com>
2018-07-18 08:54:33 +0200

    jenkins fix

--- CentOS 7.5.1804/install-jenkins-using-docker.sh
+++ CentOS 7.5.1804/install-jenkins-using-docker.sh
@@ -15,4 +15,4 @@ if ! type "docker" &> /dev/null; then
 fi
 
 # Install Jenkins to accept incoming connections at port 18080, using the given name "jenkins2-master".
-docker run --restart=on-failure:3 --rm -d -p 18080:8080 -p 50000:50000 --name jenkins2-master jenkins/jenkins
+docker run --restart=always -d -p 18080:8080 -p 50000:50000 --name jenkins2-master jenkins/jenkins
