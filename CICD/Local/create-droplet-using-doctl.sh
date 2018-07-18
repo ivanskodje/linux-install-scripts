07f79e917bb7caae9e66048e9e4702d0b6127622
Ivan Skodje <ivanskodje@protonmail.com>
2018-07-18 17:07:00 +0200

    Added a debug message to tell what the user choose.

--- CICD/Local/create-droplet-using-doctl.sh
+++ CICD/Local/create-droplet-using-doctl.sh
@@ -7,6 +7,7 @@
 if ! type "doctl" &> /dev/null; then
   echo "You need to install doctl before running this! "
   read -e -p "Do you wish to install doctl (y/n) [n]? " RESP
+  echo "You choose $RESP"
   if [ "$RESP" == "y" ] || [ "$RESP" == "Y" ]; then
     bash <(curl -Ls https://raw.githubusercontent.com/ivanskodje/linux-install-scripts/master/CentOS%207.5.1804/install-doctl.sh)
   else
