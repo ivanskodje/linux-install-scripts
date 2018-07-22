7e4f091f661b399d7a1a8fa69b9bdbf7a3444ebb
Ivan Skodje <ivanskodje@protonmail.com>
2018-07-22 17:37:47 +0200

    Added VeraCrypt install, and KeePass plugins for both Firefox and Chrome

---
+++ Ubuntu 18.4/VeraCrypt/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+APP="VeraCrypt"
+HOMEPAGE="https://www.veracrypt.fr"
+
+echo "-------------------------------"
+echo "INSTALLING: $APP"
+echo -e "WEB-SITE: $HOMEPAGE \n\n"
+
+echo "Downloading the file... "
+sudo wget https://launchpad.net/veracrypt/trunk/1.22/+download/veracrypt-1.22-setup.tar.bz2 -P ~/Downloads
+
+echo "Unpacking... "
+sudo tar -xvjf ~/Downloads/veracrypt-1.22-setup.tar.bz2
+
+echo "Installing $APP... Continue by interacting with the VeraCrypt installation window."
+sudo bash veracrypt-1.22-setup-gui-x64
+
+echo "Cleaning files... "
+sudo rm veracrypt-1.22-setup-console-x64 veracrypt-1.22-setup-console-x86 veracrypt-1.22-setup-gui-x64 veracrypt-1.22-setup-gui-x86 veracrypt-1.22-setup.tar.bz2
+
+echo -e "$APP has finished installing.\n"
