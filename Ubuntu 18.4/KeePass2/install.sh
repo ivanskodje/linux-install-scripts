06bb363e237accfde9cf6daa96c4ccb065634d61
Ivan Skodje <ivanskodje@protonmail.com>
2018-07-22 14:25:33 +0200

    KeePass2 fix with correct plugin

--- Ubuntu 18.4/KeePass2/install.sh
+++ Ubuntu 18.4/KeePass2/install.sh
@@ -6,13 +6,18 @@ echo "-------------------------------"
 echo "INSTALLING: $APP"
 echo -e "WEB-SITE: $HOMEPAGE \n"
 
-echo "Installing dependencies... (needed for KeePassHTTP Plugin)"
-sudo apt-get install mono-complete -y
+# TODO: Confirm that it works without this. If it does, remove this. Else we uncomment this to make it work.
+# echo "Adding repository..."
+# sudo add-apt-repository ppa:jtaylor/keepass -y
+# sudo apt-get update
+
+echo "Installing dependencies... "
+sudo apt-get install mono-complete curl jq -y
 
 echo "Installing $APP... "
 sudo apt-get install keepass2 -y
 
-echo "Downloading and installing KeePassHTTP plugin... "
-sudo wget https://raw.github.com/pfn/keepasshttp/master/KeePassHttp.plgx -P /usr/lib/keepass2/Plugins/
+echo "Downloading and installing plugin... "
+curl -s https://api.github.com/repos/kee-org/keepassrpc/releases/latest | jq -r ".assets[] | select(.name | test(\"KeePassRPC.plgx\")) | .browser_download_url" | xargs sudo curl -s -L -o "/usr/lib/keepass2/Plugins/KeePassRPC.plgx"
 
 echo -e "$APP has finished installing.\n\n"
